e3ad55a023a4922a90c8bd772ba82af6b5bbd137
Agbooa12 <agboolaelijah077@gmail.com>
2024-04-05 21:12:22 +0100

    the home page

--- src/User/Home.jsx
+++ src/User/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import NavBar from './NavBar'
+import style from './Styles.module.css'
 
 const Home = () => {
   return (
     <div>
       <NavBar/>
 
-      <
+      <header id={style.header}> 
+
+      </header>
+      
     </div>
   )
 }
