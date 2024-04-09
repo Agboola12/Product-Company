000cde3abf5ee66d153f9ce9b947737f83a5da43
Agbooa12 <agboolaelijah077@gmail.com>
2024-04-09 11:33:59 +0100

    wip on the design

--- src/User/ProductDetails.jsx
+++ src/User/ProductDetails.jsx
@@ -11,20 +11,19 @@ const ProductDetails = () => {
     return (
         <div>
             <NavBar/>
-            <header className='mb-5 text-white' id={style.head}>
-
-                <div className='row'>
-                    <div className='col-lg-5'>
+            <header className=' text-white' id={style.head}>
+                <div className='container'>
+                <div className='row mx-auto '>
+                    <div className='col-lg-4 mx-auto '>
+                        <p className='fw-bold mt-5' style={{fontSize: '5em'}}> WANT BIGGER <p style={{color: '#621A3F'}}> SIZE?</p>  </p>
+                        <p>Sperm Booster <br/> gives fast and <br/> long-term effect</p>
+                    </div>
+                    <div className='col-lg-4 mx-auto text-center '>
                         <img src={one} className='w-100' />
+                        <p className='fw-bold fs-1'>#25,000</p>
+                        <Link to="https://wa.link/sea1s7" className='btn btn-main-1 w-50 text-medium text-white fw-bold fs-4 rounded-0 py-3 px-4 mb-4 rounded-lg' style={{ backgroundColor: '#621A3F' }}>Order Now</Link>
                     </div>
-                    <div className='col-lg-6' style={{marginTop:'7em'}}>
-                        <p className='fs-3 fw-bold'> Product Details</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, animi? Quibusdam dicta
-                            saepe dolore qui enim, eaque temporibus necessitatibus eveniet suscipit, corporis asperiores natus voluptatem
-                            Dignissimos, sint placeat!
-                        </p>
-                        <Link to="https://wa.link/sea1s7" className='btn btn-main-1 w-50 text-medium text-white fw-bold fs-6 rounded-0 py-3 px-4 mb-4 rounded-lg' style={{ backgroundColor: '#1C7C68' }}>Get Product</Link>
-                    </div>
+                </div>
                 </div>
             </header>
 
