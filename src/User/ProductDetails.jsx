92b6a9031c49d66e08e9cee6d842bf7ecaa3390d
Agbooa12 <agboolaelijah077@gmail.com>
2024-04-09 12:20:09 +0100

    wip on the design

--- src/User/ProductDetails.jsx
+++ src/User/ProductDetails.jsx
@@ -26,12 +26,12 @@ const ProductDetails = () => {
                     </div>
 
                     <div className='mt-5'>
-                        <p className='fw-bold h1 text-center'>GIVE THEM WHAT THEY WANT <br/> WITH THE HELP OF SPERM BOOSTER</p>
+                        <p className='fw-bold h1 text-center'>GIVE THEM WHAT THEY WANT <br /> WITH THE HELP OF SPERM BOOSTER</p>
                         <div className='row mx-auto text-center'>
                             <div className='col-md-6 mx-auto'>
                                 <img src={one} className='w-100 ' />
                             </div>
-                            <div className='col-md-6 text-center fs-5 mx-auto' style={{marginTop: '7em'}}>
+                            <div className='col-md-6 text-center fs-5 mx-auto' style={{ marginTop: '7em' }}>
                                 <p>Made from nature’s most effective Maca extract, Fenugreek extract, Tongkat Ali and Horny Goat Weed and Siberian ginseng, Sperm Booster guarantees increased endurance, erection,
                                     better performance and control than ever before! Sperm Booster increases the volume of blood that fills your penis and
                                     expands its size appearing larger with every erection.
@@ -47,12 +47,21 @@ const ProductDetails = () => {
             </header>
 
             <div className='container mx-auto text-center'>
-                    <p className='fw-bold fs-1'> YOU SHOULD TRY IT! </p>
-                    <div className='row'>
-                       <div className='col-lg-4'>
-                                <p className='fw-bold'>YOU WILL BE SURPRISED TO KNOW ABOUT THE HIDDEN POWER INSIDE YOU!</p>
-                        </div>     
+                <p className='fw-bold fs-1 mt-5'> YOU SHOULD TRY IT! </p>
+                <div className='row'>
+                    <div className='col-lg-4'>
+                        <p className='fw-bold' style={{ fontSize: '4em' }}>YOU WILL BE SURPRISED TO KNOW ABOUT THE HIDDEN POWER INSIDE YOU!</p>
+                        <img src={one} className='w-100 ' />
                     </div>
+                    <div className='col-lg-4'>
+                        <img src={one} className='w-100 ' />
+                    </div>
+                    <div className='col-lg-4'>
+                        <p>Sex anytime and anywhere: On the table, in the car, </p>
+                        <p className='fw-bold'>in the cinema hall, </p>
+                        <p className='fw-bold' style={{ color: '#621A3F' }}>OR IN BED AT HOME. </p>
+                    </div>
+                </div>
             </div>
 
             <FooterBar />
